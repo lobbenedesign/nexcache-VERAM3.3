3fa894ab3c8574c584fe1848274f7061902d2fbb
Giuseppe Lobbene <lobbenedesign@gmail.com>
2026-03-12 09:45:56 +0100

    Build Fix: Resolve macro redefinitions and fix release header generator to use NexCache prefixes.

--- src/version.h
+++ src/version.h
@@ -6,13 +6,13 @@
 #define SERVER_TITLE "NexCache"
 #define NEXCACHE_VERSION "1.0.0"
 #define NEXCACHE_VERSION_NUM 0x00010000
+
 /* The release stage is used in order to provide release status information.
  * In stable branch the status is always "dev".
  * During release process the status will be set to rc1,rc2...rcN.
  * When the version is released the status will be "ga". */
 #define NEXCACHE_RELEASE_STAGE "ga"
 
-/* NexCache OSS compatibility version, should never
- * exceed 7.2.x. */
-#define NEXCACHE_VERSION "7.2.4"
-#define NEXCACHE_VERSION_NUM 0x00070204
+/* NexCache OSS compatibility version for clients that check for specific versions. */
+#define NEXCACHE_COMPAT_VERSION "7.2.4"
+#define NEXCACHE_COMPAT_VERSION_NUM 0x00070204
